5fa202dbe1ccee55d0913a95ea34de282e5a7a35
kartik <patelkartik18112000@gmail.com>
2024-03-28 22:45:57 +0530

    Update Header.jsx
    Chnaged css for logout button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -88,7 +88,7 @@ const Header = () => {
                      <div className="nav__btns d-flex align-items-center gap-2">
                         {
                            user ? <> <h5 className='mb-0'>{user.username}</h5>
-                                 <Button className='btn btn-dark' onClick={logout}>Logout</Button>
+                                 <Button className='btn btn-dark text-black' onClick={logout}>Logout</Button>
                               </> : <>
                                  <Button className='btn secondary__btn'><Link to='/login'>Login</Link></Button>
                                  <Button className='btn primary__btn'><Link to='/register'>Register</Link></Button>
